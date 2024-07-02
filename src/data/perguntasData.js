51100d6b48a7123923beff99e9a86d3b84d79296
lmzanini <lm_zanini@hotmail.com>
2024-07-02 20:18:50 -0300

    perguntas adicionadas

--- src/data/perguntasData.js
+++ src/data/perguntasData.js
@@ -4,16 +4,16 @@ const faqsData = [
     pergunta: "O Axadinhos é confiável?",
     resposta: "Sim. O Axadinhos é confiável. Nossa missão é achar os melhores produtos da Shoppee, não vendemos nada, apenas achamos os melhores produtos e disponibilizamos."
   },
-  // {
-  //   id: 2,
-  //   pergunta: "Posso alterar meu endereço de entrega?",
-  //   resposta: "Sim, você pode alterar seu endereço de entrega na seção de configurações da sua conta."
-  // },
-  // {
-  //   id: 3,
-  //   pergunta: "Quais métodos de pagamento são aceitos?",
-  //   resposta: "Aceitamos cartões de crédito, débito e PayPal como métodos de pagamento."
-  // },
+  {
+    id: 2,
+    pergunta: "Por que o Axadinhos é bom?",
+    resposta: "O Axadinhos seleciona os melhores produtos e disponibiliza para todos para que não precise perder tempo."
+  },
+  {
+    id: 3,
+    pergunta: "O Axadinhos é responsável por alguma compra?",
+    resposta: "Não. O Axadinhos somente acha os melhores produtos da Shoppee e disponibiliza. Toda a responsabilidade é da Shoppee e do vendedor."
+  },
   // {
   //   id: 4,
   //   pergunta: "Como posso rastrear meu pedido?",
